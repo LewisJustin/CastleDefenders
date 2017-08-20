a45688ed385337d1545be62c44f8c0a477cf0032
LongKniife5 <31137673+LongKniife5@users.noreply.github.com>
2017-08-20 10:02:44 -0600

    hi

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -3,51 +3,70 @@ using System.Collections;
 
 public class EnemyLogic : MonoBehaviour
 {
-	[SerializeField] private Transform toDestory;
+	[SerializeField] private GameObject toDestory;
 
 	public float speed = 1f;
-	public int health;
+	public int health= 100;
 	public bool ranged;
+	private bool hasArrived = false;
+
+	private void Awake()
+	{
+		health = 100;
+	}
 
 	void Update()
 	{
-		if (!ranged)
+		if (health <= 0)
+			Destroy(toDestory);
+
+		#region GettingToLocation
+		if (!ranged && !hasArrived)
 		{
-			if (toDestory.position.x < 5)
+			if (transform.position.x < 5)
 			{
 				transform.Translate(Vector3.right * speed * Time.deltaTime);
 			}
 			else
 			{
 				StartCoroutine(startAttackingMelee());
+				hasArrived = true;
 			}
 		}
 
-		if (ranged)
+		if (ranged && !hasArrived)
 		{
-			if (toDestory.position.x < 8)
+			if (transform.position.x < 8)
 			{
 				transform.Translate(Vector3.right * speed * Time.deltaTime);
 			}
 			else
 			{
 				StartCoroutine(startAttackingRanged());
+				hasArrived = true;
 			}
 		}
+		#endregion
 	}
 
 	IEnumerator startAttackingMelee()
 	{
-		Debug.Log("attack melee");
+		while (health > 0)
+		{
+			Debug.Log("attack melee");
 
-		yield return new WaitForSeconds(3f);
+			yield return new WaitForSeconds(3f);
+		}
 	}
 
 	IEnumerator startAttackingRanged()
 	{
-		Debug.Log("attack ranged");
+		while (health > 0)
+		{
+			Debug.Log("attack ranged");
 
-		yield return new WaitForSeconds(3f);
+			yield return new WaitForSeconds(3f);
+		}
 	}
 	
 }
