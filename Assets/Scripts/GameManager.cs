fbc5e9691c396ae7137d5484c1121377d8a8873c
Sander Olsen Hoel <sander.hoel@hotmail.com>
2017-08-20 13:19:46 +0200

    Fixed the Menu(again) and Made text look alot better

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
 
     public void EndGame()
-    { 
+    {
         //Quit the Game
         Application.Quit();
         Debug.Log("End Game if in a build");
