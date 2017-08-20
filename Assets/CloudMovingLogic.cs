fec87f05fb8a24df0c491316a463d8df91ea6237
LongKniife5 <31137673+LongKniife5@users.noreply.github.com>
2017-08-20 07:48:45 -0600

    cloud moving fixes

---
+++ Assets/CloudMovingLogic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CloudMovingLogic : MonoBehaviour
+{
+	[SerializeField] private GameObject toDestory;
+
+	void Update()
+	{
+		transform.Translate(Vector3.right * Random.Range(5f, 6f) * Time.deltaTime);
+
+		if (transform.position.x > 30)
+			Destroy(toDestory);
+	}
+}
