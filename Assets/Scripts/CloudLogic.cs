6436e96b20ef8d0ffb65d91cd0c378ba8a2fb198
LongKniife5 <31137673+LongKniife5@users.noreply.github.com>
2017-08-20 06:29:06 -0600

    update
    trying to figure out why instantiating the cloud is not working.

---
+++ Assets/Scripts/CloudLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudLogic : MonoBehaviour {
+
+	[SerializeField] private Transform cloudSpawnPoints;
+	[SerializeField] private Sprite cloudSprite;
+
+	void Start ()
+	{
+		Transform _sp = cloudSpawnPoints;
+		Instantiate(cloudSprite, _sp.position, _sp.rotation);
+	}
+}
