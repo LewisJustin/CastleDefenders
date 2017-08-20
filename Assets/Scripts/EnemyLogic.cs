e5ae227a8fdd1df958ab6467f40a72b9b03f2038
LongKniife5 <31137673+LongKniife5@users.noreply.github.com>
2017-08-20 09:07:57 -0600

    update+menufix

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -4,13 +4,16 @@ public class EnemyLogic : MonoBehaviour
 {
 	[SerializeField] private GameObject toDestory;
 
-		public float speed = 1f;
-		public int health;
-		public bool ranged;
+	public float speed = 1f;
+	public int health;
+	public bool ranged;
+	public bool isInRange = false;
 
 	void Update()
 	{
-		transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+		while(!isInRange)
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
 
 	}
 }
