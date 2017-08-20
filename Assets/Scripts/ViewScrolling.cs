4af75c5df5a717e8423e9c73bc9c368c339ad7de
LongKniife5 <31137673+LongKniife5@users.noreply.github.com>
2017-08-19 21:01:37 -0600

    VeiwScrolling

---
+++ Assets/Scripts/ViewScrolling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ViewScrolling : MonoBehaviour
+{
+	private void FixedUpdate()
+	{
+		if (Input.GetAxis("Mouse ScrollWheel") != 0f) // forward
+		{
+			if(transform.position.x <= 10 && transform.position.x >= -10)
+				transform.Translate(new Vector3 (-Input.GetAxis("Mouse ScrollWheel") * 500f * Time.deltaTime, 0f,0f));
+
+			if (transform.position.x < -10)
+				transform.Translate(new Vector3(.1f, 0f, 0f));
+
+			if (transform.position.x > 10)
+				transform.Translate(new Vector3(-.1f, 0f, 0f));
+		}
+	}
+}
