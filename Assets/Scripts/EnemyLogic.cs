2debf05a44530888c448c6d3bc97fe44ffa12621
Sander Olsen Hoel <sander.hoel@hotmail.com>
2017-08-20 17:56:31 +0200

    Revert "Merge branch 'master' of https://github.com/LongKniife5/CastleDefenders"
    This reverts commit ae1cf6d7d73b4f914a6f81b4c5e176a87d084d78, reversing
    changes made to 2c82a6d05f0adda1fede000e1d4cdcd679e97d26.

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class EnemyLogic : MonoBehaviour
 {
@@ -12,42 +11,10 @@ public class EnemyLogic : MonoBehaviour
 	void Update()
 	{
 		if (!ranged)
-		{
 			if (toDestory.position.x < 5)
-			{
 				transform.Translate(Vector3.right * speed * Time.deltaTime);
-			}
-			else
-			{
-				StartCoroutine(startAttackingMelee());
-			}
-		}
-
 		if (ranged)
-		{
 			if (toDestory.position.x < 8)
-			{
 				transform.Translate(Vector3.right * speed * Time.deltaTime);
-			}
-			else
-			{
-				StartCoroutine(startAttackingRanged());
-			}
-		}
 	}
-
-	IEnumerator startAttackingMelee()
-	{
-		Debug.Log("attack melee");
-
-		yield return new WaitForSeconds(3f);
-	}
-
-	IEnumerator startAttackingRanged()
-	{
-		Debug.Log("attack ranged");
-
-		yield return new WaitForSeconds(3f);
-	}
-	
 }
