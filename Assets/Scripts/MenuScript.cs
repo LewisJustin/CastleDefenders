9596d024841776ba9a57b9199fc3175d7ed9a36f
LongKnife5 <JD3565@k12.sd.us>
2017-09-16 12:27:02 -0600

    Game Pauses when you pause

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -18,8 +18,22 @@ public class MenuScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             menu = !menu;
+           
+           StartCoroutine(SetTimeRate());
         }
+
+        if(!menu)
+            Time.timeScale = 1f;
 
         animator.SetBool("Menu", menu);
 	}
+
+    IEnumerator SetTimeRate()
+    {
+         if(menu)
+         {
+            yield return new WaitForSeconds(.7f);
+            Time.timeScale = 0;
+         }
+    }
 }
